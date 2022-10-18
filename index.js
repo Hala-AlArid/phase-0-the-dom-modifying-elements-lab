3239903ca0bf65a87d8633ba8e9dc47f919742c2
Hala AlArid <halaalarid99@gmail.com>
2022-10-19 01:19:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const newHeader = document.createElement("h1");
+
+document.body.append(newHeader);
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Hala is the champion';
